c7711c3eaa9468cd64dc4dcb3437115445639e5a
Drashti Limbachiya <drashtil92@gmail.com>
2017-03-03 13:15:29 -0800

    final code for calculator

--- main.py
+++ main.py
@@ -7,12 +7,24 @@ previous = 0
 run = True
 
 def performMath():
+    global run
+    global previous
+    equation = ""
+    if previous == 0:
+        equation = raw_input("Enter equation:")
+    else:
+        equation = raw_input(str(previous))
 
-    equation = input("Enter equation:")
     if equation == 'quit':
+        print("Goodbye!")
         run = False
     else:
-        print("You typed", equation)
+        equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
+
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous)+ equation)
 
 while run:
     performMath()
