d22f3f384ab48a8ce9d05ca2506af0abe70f3c4d
Drashti Limbachiya <drashtil92@gmail.com>
2017-03-02 21:35:20 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import re
+
+print("Calculator")
+
+previous = 0
+run = True
+
+def performMath():
+    equation = input("Enter equation:")
+    print("Your Typed", equation)
+
+while run:
+    performMath()
